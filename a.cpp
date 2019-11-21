c564c726843c44493dbd7a80a7af3a8e907a416e
Krabs <zhenghongye2000@gmail.com>
2019-11-21 08:48:39 +0800

    adad

--- a.cpp
+++ a.cpp
@@ -1,23 +1,33 @@
 #include<iostream>
 #include<vector>
+#include<map>
+#include <string>
 using namespace std;
 
 
 int main(){
-    string s="";
-    string res ="";
-    res = BacktoCppAgain(s);
-    cout<<"adad"<<endl;
+    // string s="";
+    // string res ="";
+    // res = BacktoCppAgain(s);
+    // cout<<"adad"<<endl;
+    map<string,int> m;
+    m["ad"]=123;
+    std::map<std::string,int>::iterator it = m.begin();
+    for(;it != m.end();it++){
+        std::cout << it->first <<endl;
+        std::cout << it->second << endl;
+    }
+    return 0;
 }
 
-string& BacktoCppAgain(string &s){
+string BacktoCppAgain(string s){
     s ="cppcpp";
     return s;
 }
 
 template <typename T> void display_message(const string &str,const vector<T> &v){
-    for(i=0;i<v.size();i++){
-        T t= = v[i];
-        count << t << "";
+    for(int i=0;i<v.size();i++){
+        T t=  v[i];
+        cout << t << "";
     }
 }
